6ae19763eabfc5cc7d8fac5f33e1bc49b17ffe8c
Emily Brumfield <belac54@hotmail.com>
2018-11-24 22:02:36 -0500

    process2e deletes unnecessary stats

--- index.js
+++ index.js
@@ -5,84 +5,113 @@
 
 /* OLD CODE FROM THE OTHER PROCESSOR BELOW */
 
-//At this point, most of the coding stuff works and I need to work on figuring out the game design stuff.
-//Need to figure out a damage conversion formula of some sort.
-
-//Inefficiencies
-//getStatsRegex and getLineRegex seem kinda kludgy, but they work for now
-
-//Compatible monster sources: PFSRD
-//can refactor this for efficiency but let's just work on making things work for now
-//should probably write some unit tests for all this soon
-
-//New Priority: Now that the basics work, pick a converter type (2e, 5e, etc) and focus on getting everything it needs
-//Worry about other stuff later
-
 const inputBox = "input-box";  //the name of the input textarea; paste a monster here from a compatible source
 const outputBox = "output-box";  //the name of the output textarea
 const armorHalf = 17; //any Armor Class points over this amount are halved and rounded down; used for converting AC from PF/3e to 2e
 
-//-----------------------------TEST FUNCTION, IGNORE-------------------
-
-function testProcess() {
-    let testMonster = butcherMonster(inputBox);
-    let testName = getStats(testMonster, "CR");
-    testName = processName(testName);
-    let testAC = getStats(testMonster, "AC");
-    testAC = processAC(testAC);
-    let testHP = getStats(testMonster, "hp");
-    testHP = processHitDice(testHP);
-    let testSaves = getStats(testMonster, "Fort");
-    let testAbilities = getStats(testMonster, "Str");
-    testAbilities = processAbilities(testAbilities);
-    let testAlignment = getStatsRegex(testMonster, /LG|NG|CG|LN|\bN\b|LE|NE|CE/);
-    testAlignment = processIdentity(testAlignment);
-    let testDefensive = getStats(testMonster, "Defensive Abilities");
-    let testWeakness = getStats(testMonster, "Weaknesses");
-    let testSpecialAttacks = getStats(testMonster, "Special Attacks");
-    let testMelee = getStats(testMonster, "Melee");
-    testMelee = processAttack(testMelee);
-
-    clearText();
-    addTextLine(testName);
-    addTextLine(testAC.AC);
-    addTextLine(testHP);
-    addTextLine(testSaves);
-    addTextLine(testAbilities.strength);
-    addTextLine(testAlignment.sizeCategory + " "); addText(testAlignment.creatureCategory);
-    addTextLine(testMelee);
-}
-
-
-function addText(textchunk){  //adds text to the last line in the output box
-    let oldText = document.getElementById(outputBox).value;
-    oldText += textchunk;
-    document.getElementById(outputBox).value = oldText;
-}
-
-function addTextLine(textchunk){ //adds text on a new line in the output box; adds text without new line if output box is empty
-    let oldText = document.getElementById(outputBox).value;
-    if (oldText != "") {
-        oldText += "\n" + textchunk;
-    }
-    else {
-        oldText += textchunk;
-    }
-    document.getElementById(outputBox).value = oldText;
-}
-
-function addTextLineConditional(rawText) {  //adds a textline only if the string isn't empty
-    if (rawText != "") {
-        addTextLine(rawText)
-    }
-}
-
 //----STRING FUNCTIONS
 //----Various functions to get text from the input, split it up into arrays, search it for particular information
 
-function splitText(targetID) {
-    let textchunk = grabText(targetID);
+function test() {
+    //creates newArray by grabbing text from the input textarea and splitting it
+    //splitting the text into an array makes it much easier to work with
+
+    let newArray = splitText(inputBox);
+    newArray = convert2e(newArray);
+
+    mergeText(newArray); //joins the array back into a string and sends the results to the output textarea
+}
+
+function convert2e(newArray) {
+    //takes a string array, converts stats from Pathfinder to AD&D 2e rules, returns the result
+    //most of this is simple RegExp-based deletion of unnecessary stats, but there's some conversions as well
+
+    //Step One: Remove unnecessary text
+    //currently repeating myself; will use functions later
+
+    //DELETE CHALLENGE RATING
+    if (ifExists("CR", newArray)) {
+        let targetLine = getStats("CR",  newArray);
+        let targetIndex = findLine("CR", newArray);
+        targetLine = targetLine.replace(/ *CR \d*/g, "");  //deletes CR, the following number, and any spaces nearby
+        newArray[targetIndex] = targetLine;
+    }
+
+    //DELETE EXPERIENCE LINE ENTIRELY
+    if (ifExists("XP", newArray)) {
+        let targetIndex = findLine("XP", newArray);
+        newArray.splice(targetIndex, 1);
+    }
+
+    //DELETE INITIATIVE
+    if (ifExists("Init", newArray)) {
+        let targetLine = getStats("Init",  newArray);
+        let targetIndex = findLine("Init", newArray);
+        targetLine = targetLine.replace(/ *Init *[+-]\d* *;* */g, "");  //deletes init, the following number, and any spaces nearby
+        newArray[targetIndex] = targetLine;
+    }
+
+    //DELETE PERCEPTION
+    if (ifExists("Perception", newArray)) {
+        let targetLine = getStats("Perception",  newArray);
+        let targetIndex = findLine("Perception", newArray);
+        targetLine = targetLine.replace(/ *;* *Perception *[+-]\d* *;* */g, "");
+        newArray[targetIndex] = targetLine;
+    }
+
+    //DELETE SPACE/REACH LINE ENTIRELY
+    if (ifExists("Reach", newArray)) {
+        let targetIndex = findLine("Reach", newArray);
+        newArray.splice(targetIndex, 1);
+    }
+
+    //DELETE BASE ATTACK BONUS, CMB, CMD LINE ENTIRELY
+    if (ifExists("CMB", newArray)) {
+        let targetIndex = findLine("CMB", newArray);
+        newArray.splice(targetIndex, 1);
+    }
+
+    //DELETE FEAT LINE ENTIRELY
+    if (ifExists("Feats", newArray)) {
+        let targetIndex = findLine("Feats", newArray);
+        newArray.splice(targetIndex, 1);
+    }
+
+    //DELETE SKILL LINE ENTIRELY
+    if (ifExists("Skills", newArray)) {
+        let targetIndex = findLine("Skills", newArray);
+        newArray.splice(targetIndex, 1);
+    }
+
+
+
+
+
+    return newArray;
+}
+
+function ifExists(searchWord, targetArray) {  
+    //searches an array for a line that contains the searchWord; returns true if it finds anything
+
+    if (findLine(searchWord, targetArray) > -1) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+function splitText() {
+    //gets the text from inputBox, fixes special characters like funky dashes and such, and splits it into an array
+    let textchunk = document.getElementById(inputBox).value;
+    textchunk = sanitize(textchunk);
     return textchunk.split("\n");
+}
+
+function mergeText(targetArray) {
+    //merges the text from an array and puts it into outputBox
+    let textchunk = targetArray.join("\n");
+    document.getElementById(outputBox).value = textchunk
 }
 
 function wholeWord(word) { //placeholder
@@ -90,12 +119,14 @@ function wholeWord(word) { //placeholder
     return regWord;
 }
 
-function findLine(word, monster) {  //finds a particular element in a monster array based on the starting word, returns index of that element
-    //this will stop after the first time it finds the element
+function findLine(word, targetArray) {
+    //finds the first element in an array that has a certain word; returns that element index
+    //if there's nothing that matches, returns -1
+
     let targetIndex = -1;
 
-    for (let i = 0; i < monster.length; i += 1) {
-        if ( wholeWord(word).test(monster[i]) ) { //if the word is in the particular line of the array
+    for (let i = 0; i < targetArray.length; i += 1) {
+        if ( wholeWord(word).test(targetArray[i]) ) { //if the word is in the particular line of the array
             targetIndex = i;
             break;
         }
@@ -105,8 +136,8 @@ function findLine(word, monster) {  //finds a particular element in a monster ar
     return targetIndex;
 }
 
-function findLineRegex(word, monster) {  //just like findLine, but takes a RegExp instead of a normal string
-    //this will stop after the first time it finds the element
+function findLineRegex(word, monster) {  
+    //as findLine, but searches for a regex
     let targetIndex = -1;
 
     for (let i = 0; i < monster.length; i += 1) {
@@ -169,39 +200,10 @@ function clearModifiers(rawText) {  //deletes modifiers like +3, -5, or the like
     return rawText;
 }
 
-//----SOURCE CHECK
-//----Identifies source of the monster text; can only process a compatible source or something formatted the same
-
-function checkPFSRD(textchunk) {
-    //if first appearances of CR comes before XP, and XP comes before Init, then it must be a monster from PFSRD.org
-    //if not, it's probably some other format
-
-    //first, we need to make sure all the phrases are in the text sample in the first place; otherwise it's definitely not a PFSRD entry
-    if ( wholeWord("CR").test(textchunk) && wholeWord("XP").test(textchunk) && wholeWord("Init").test(textchunk) ) {
-        //if all three are in the text, can proceed to check order to make sure it's a PFSRD entry and not something else; capitalization matters
-        //defining the next three terms as variables for readability
-        let positionCR = textchunk.search(wholeWord("CR"));
-        let positionXP = textchunk.search(wholeWord("XP"));
-        let positionInit = textchunk.search(wholeWord("Init"));
-
-        if (positionCR < positionXP && positionXP < positionInit) {
-            return true;
-        }
-        else {
-            return false;
-        }
-
-    } else {
-        //if all three phrases aren't in the text, it's not a PFSRD entry, so return false
-        return false;    
-    }
-
-}
-
 //----GET STATS
 //----Functions to grab particular stat lines from the document; most can be done with getStats
 
-function getStats(monster, statName) {  //searches a "monster" array for a line that contains the relevant statName; if none, returns ""
+function getStats(statName, monster) {  //searches a "monster" array for a line that contains the relevant statName; if none, returns ""
     let targetIndex = findLine(statName, monster);
     let monsterStat = "";
     if (targetIndex > -1) {
@@ -444,128 +446,4 @@ function convert2eTHAC0(HitDice) {  //calculates THAC0 according to standard rul
     };
 
     return THAC0;
-}
-
-//---NEEDS BETTER DEFINITION
-//---Various stuff that should be renamed and put elsewhere
-
-//makes sure this is PFSRD text, chops it up for use in the next step
-
-function butcherMonster(targetID) {
-    if ( checkPFSRD(grabText(inputBox))) {
-        //here is where the code goes
-        return splitText(inputBox);
-    }
-    else {
-        alert("This isn't going to work out. Not PFSRD.")
-        return ["Not PFSRD"];
-    }
-}
-
-/*Summary of procedures:
---Check if the text came from a standard Pathfinder PFSRD page
---Break down the text into chunks of information; this will take quite some time to work out
-*/
-
-/*PLANS
---Each format (PFSRD, etc) is going to need slightly different mechanisms due to different rules and different layouts
---Probably easiest to make separate functions to harvest each type of data, like getAC, getAttacks, etc.
---Might be efficient to delete any array element that's already been harvested completely, like the AC line
---Can have special processors and converters available in each harvester function
-
-*/
-
-function convert2e() { //converts to AD&D 2e standards
-
-    let monster = butcherMonster(inputBox);
-    
-    let monsterName = getStats(monster, "CR");
-    monsterName = processName(monsterName);
-
-    let monsterAbilities = getStats(monster, "Str");
-    monsterAbilities = processAbilities(monsterAbilities);
-    let monsterIdentity = getStatsRegex(monster, /LG|NG|CG|LN|\bN\b|LE|NE|CE/);
-    monsterIdentity = processIdentity(monsterIdentity);
-
-    let monsterHD = getStats(monster, "hp");
-    monsterHD = processHitDice(monsterHD);
-
-    let monsterAC = getStats(monster, "AC");
-    monsterAC = processAC(monsterAC);
-    monsterAC.AC = convert2eAC(monsterAC.AC);
-
-    let monsterSpeed = getStats(monster, "Speed");
-
-
-    //asterisks mean that the 3e/PF version of the attack had iterative followups, but there's no clear 2e equivalent
-    //best solution might be to apply the fighter melee attack rates, but I'm not sure
-    //some traits don't exist on all monsters; for these, we're going to set their default value to "" and only give them another value if one exists
-    //probably should turn these into functions for simplicity later
-
-    let monsterMelee = getStats(monster, "Melee");;  
-    if (monsterMelee != "") {
-        monsterMelee = processAttack(monsterMelee);
-        monsterMelee = monsterMelee.replace(" iterative", "*")
-    }
-
-    let monsterRanged = getStats(monster, "Ranged");;  
-    if (monsterRanged != "") {
-        monsterRanged = processAttack(monsterRanged);
-        monsterRanged = monsterRanged.replace(" iterative", "*")
-    } 
-
-    let monsterDefensive = getStats(monster, "Defensive Abilities");
-    if (monsterDefensive === "") {
-        monsterDefensive = getStats(monster, "Immune");
-    }
-    //because sometimes there's Defensive Abilities with immunity in it, sometimes there's just Immune
-
-    let monsterSpecialAttacks = getStats(monster, "Special Attacks");
-
-    let monsterWeaknesses = getStats(monster, "Weaknesses");
-    if (monsterWeaknesses === "") {
-        monsterWeaknesses = getStats(monster, "Weakness");
-    }
-    //because sometimes it's Weaknesses and sometimes it's Weakness
-
-    let monsterSQ = getStats(monster, "SQ");
-
-    //spells are here; doing each one as a separate entry to make it easier to change the formatting or order later
-    let monsterAtWill = getStats(monster, "At will");
-    let monster3Day = getStats(monster, "3/day");
-    let monster2Day = getStats(monster, "2/day");
-    let monster1Day = getStats(monster, "1/day");
-    let monster1Week = getStats(monster, "1/week");
-    let monster1Month = getStats(monster, "1/month");
-    let monster1Year = getStats(monster, "1/year");
-    
-    clearText();
-    addTextLine(monsterName);
-    addText(" (" + monsterIdentity.sizeCategory + " " + monsterIdentity.creatureCategory + ")" )
-    addTextLine("HD " + monsterHD + ", ");
-    addText("AC " + monsterAC.AC + ", ");
-    addText("THAC0 " + convert2eTHAC0(monsterHD) + ", ");    
-    addText(monsterSpeed);  //2e normally uses MV, but late 2e is closer to 3e/PF speed and that's probably a better choice here
-    if (monsterMelee != "") {
-        addTextLine("Melee: " + monsterMelee);    
-    }
-
-    if (monsterRanged != "") {
-        addTextLine("Ranged: " + monsterRanged);
-    }       
-    
-    addTextLine(""); //extra whitespace for readability
-    
-    //add the next few traits only if they exist
-    addTextLineConditional(monsterSpecialAttacks);
-    addTextLineConditional(monsterDefensive);
-    addTextLineConditional(monsterSQ);
-    addTextLine(""); //extra whitespace for readability
-    addTextLineConditional(monsterAtWill);
-    addTextLineConditional(monster3Day);
-    addTextLineConditional(monster2Day);
-    addTextLineConditional(monster1Day);
-    addTextLineConditional(monster1Week);
-    addTextLineConditional(monster1Month);
-    addTextLineConditional(monster1Year); 
 }
